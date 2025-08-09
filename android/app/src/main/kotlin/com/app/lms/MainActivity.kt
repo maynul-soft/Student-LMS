75aa03ba26ba961d5270e56a488db5cb06729e3a
Maynul Islam Jibon <maynulislamjibon4@gmail.com>
2025-08-09 20:27:33 +0600

    make change on my activity kt

--- android/app/src/main/kotlin/com/app/lms/MainActivity.kt
+++ android/app/src/main/kotlin/com/app/lms/MainActivity.kt
@@ -1,5 +1,13 @@
-package com.bangla.lms
+package com.app.lms
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // System UI কে edge-to-edge করার জন্য নতুন মেথড
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
