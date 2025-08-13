3ea368b387755303c96591897839a40a5a58c462
Maynul Islam Jibon <maynulislamjibon4@gmail.com>
2025-08-13 20:13:19 +0600

    fix app stability bugs

--- android/app/src/main/kotlin/com/app/lms/MainActivity.kt
+++ android/app/src/main/kotlin/com/app/lms/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.app.lms
+package com.bangla.lms
 
 import android.os.Bundle
 import androidx.core.view.WindowCompat
@@ -7,7 +7,6 @@ import io.flutter.embedding.android.FlutterActivity
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // System UI কে edge-to-edge করার জন্য নতুন মেথড
         WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 }
